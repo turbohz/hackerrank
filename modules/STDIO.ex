18b56f9edb3f5a18d4bd87e75c4e56cf5488627d
Gerard Ruiz <gerard@mailtrack.io>
2017-01-24 12:17:58 +0100

    Adds generic module to handle IO

---
+++ modules/STDIO.ex
@@ -0,0 +1,12 @@
+defmodule STDIO do
+    def read, do: read []
+    def read(lines) do
+      case IO.read(:stdio, :line) do
+        :eof -> lines
+        {:error, reason} -> IO.puts "Error: #{reason}"
+        data ->
+          lines = lines ++ [(data |> String.trim_trailing)]
+          read lines
+      end
+    end
+end
